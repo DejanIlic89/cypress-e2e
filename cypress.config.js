c7643e4a46be582f5dc08af0944940974160b8a8
Dejan Ilic <theekey@gmail.com>
2024-08-12 00:29:57 +0200

    gh pages added report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  video: true,
+  // reporter: 'cypress-mochawesome-reporter',
+  // video: true,
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      reportFilename: 'report',
+      overwrite: false,
+      html: true,
+      json: true
+    },
     baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
       // implement node event listeners here
